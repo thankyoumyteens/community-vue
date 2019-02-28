ad23d5cc201ba3d44578d048ac4cc7b838148a28
zhaoshengzhi <iloveyesterday@outlook.com>
2019-02-28 15:33:32 +0800

    init

---
+++ src/components/Split/Split.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="split" ref="split"></div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      size: {
+        type: Number,
+        defaultValue: 1.0
+      }
+    },
+    watch: {
+      size () {
+        this.setHeight()
+      }
+    },
+    mounted () {
+      this.setHeight()
+    },
+    methods: {
+      /**
+       * 自定义高度
+       */
+      setHeight () {
+        if (this.size) {
+          this.$refs.split.style.height = this.size + 'rem'
+        } else {
+          this.$refs.split.style.height = '1.0rem'
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .split
+    width 100%
+    height 1rem
+    border-top 0.05rem solid rgba(7, 17, 27, 0.1)
+    border-bottom 0.05rem solid rgba(7, 17, 27, 0.1)
+    background #f3f5f7
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     <v-header/>
+    <split size="0.5"/>
   </div>
 </template>
 
 <script>
   import VHeader from '../components/Header/Header'
+  import Split from '../components/Split/Split'
 
   export default {
     components: {
-      VHeader
+      VHeader,
+      Split
     }
   }
 </script>
